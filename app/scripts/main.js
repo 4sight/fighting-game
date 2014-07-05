aceb61ff42e181c28be96875c7ab6e4b9890defd
Greeley Dawson <hgd4th@gmail.com>
2014-07-05 17:55:43 -0400

    Javascript setup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('The Iron Yard Rocks');
+// Constructors
+
+var Player = function(attributes) {
+	var attributes = || {};
+	this.name = attributes.name;
+	this.health = 100;
+	this.attack = function(target) {
+		attackResult(this, target);
+	};
+	this.element = attributes.element;
+};
+
+var Enemy = function(attributes) {
+	var attributes = attributes || {};
+	this.name = attributes.name;
+	this.health = 100;
+	this.element = attributes.element;
+};
+
+// Create player
+
+var 
+
+'Name your fighter'
